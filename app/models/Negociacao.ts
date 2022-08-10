380dda7da9144985a80b9cbbe117f966e1575a26
Mateus Souza <mateus_souza11@hotmail.com.br>
2022-08-10 17:13:46 -0300

    Transformando o codigo para typescript

--- app/models/Negociacao.ts
+++ app/models/Negociacao.ts
@@ -1,14 +1,28 @@
-class Negociacao {
+export class Negociacao {
     
-    #data;
-    #quantidade;
-    #valor;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
 
-    constructor(data, quantidade, valor) {
-        this.#data = data;
-        this.#quantidade = quantidade;
-        this.#valor = valor;
+    constructor(data: Date, quantidade: number, valor: number) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
     }
 
-    get
+    get data(): Date {
+        return this._data;
+    }
+
+    get quantidade(): number {
+        return this._quantidade
+    }
+
+    get valor(): number {
+        return this._valor
+    }
+
+    get volume(): number {
+        return this._quantidade * this._valor;
+    }
 }
